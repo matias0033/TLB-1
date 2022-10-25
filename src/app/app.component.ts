bc78517afac579f03523d808ab688595b2958eff
bastidiazp <basti.diazp@duocuc.cl>
2022-10-25 13:13:01 -0300

    GEOLOCATION CONSOLA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx'; /*1*/
+import { NavController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
+
 export class AppComponent {
-  constructor() {}
+  constructor(
+    protected platform: Platform,
+    //protected navController: NavController,
+    //public LocalStorage: StorageService,
+
+    private geolocation: Geolocation) { /*3*/
+    this.platform.ready().then(async () => {
+      //await this.LocalStorage.init();
+      this.getGeolocation();
+    }); /*5*/
+  }
+
+
+
+  /* @@@@@ GEOLOCATION @@@@@ (NO TOCAR) */
+  getGeolocation() { /*2*/
+
+    this.geolocation.getCurrentPosition().then((resp) => {
+
+      console.log("resp ", resp); /*4*/
+      // resp.coords.latitude
+      // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      // data can be a set of coordinates, or an error (if an error occurred).
+      // data.coords.latitude
+      // data.coords.longitude
+    });
+  }
 }
+
+/* 1.- IMP. para Geolocation */
+/* 2.- IMP. para Geolocation */
+/* 3.- IMP. para Geolocation */
+/* 4.- IMP. para Geolocation */
+/* 5.- IMP. para Geolocation */
+
+// SINTAXIS SUS REVISAR POSTERIORMENTE
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,18 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { enterAnimation } from './animations/nav-animations'; /*1*/
+import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx'; /*4*/
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot({ navAnimation: enterAnimation }), AppRoutingModule], /*2*/
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }, Geolocation], /*3*/
   bootstrap: [AppComponent],
 })
 export class AppModule { }
 
 /* 1.- IMP. para Animaciones */
 /* 2.- IMP. para Animaciones */
+
+/* 3.- IMP. para Geolocation */
+/* 4.- IMP. para Geolocation */
